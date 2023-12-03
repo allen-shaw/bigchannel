c5777cfea57ec97237005a0b4fcaa84236d61294
allen <allenshaw19@gmail.com>
2023-12-03 21:15:52 +0800

    test(demo): add producer/consumer first test

--- mock/client/exmaple_test.go
+++ mock/client/exmaple_test.go
@@ -29,7 +29,23 @@ func TestProduceConsumeSameTime(t *testing.T) {
 }
 
 func TestProduceFirst(t *testing.T) {
+	addr := "127.0.0.1:18088"
+	client, err := NewClient(addr)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("new client succ")
 
+	/*go*/
+	startProducer(client)
+	go startConsumer(client)
+
+	time.Sleep(10 * time.Second)
+
+	fmt.Println("send msgs:")
+	fmt.Println(strings.Join(sendMsgs, "; "))
+	fmt.Println("recv msgs:")
+	fmt.Println(strings.Join(recvMsgs, "; "))
 }
 
 func TestProduceFirstThenConsumeThenProduceAgain(t *testing.T) {
@@ -37,7 +53,24 @@ func TestProduceFirstThenConsumeThenProduceAgain(t *testing.T) {
 }
 
 func TestConsumeFirst(t *testing.T) {
+	addr := "127.0.0.1:18088"
+	client, err := NewClient(addr)
+	if err != nil {
+		panic(err)
+	}
+	log.Println("new client succ")
 
+	/*go*/
+	go startConsumer(client)
+	time.Sleep(3 * time.Second)
+	startProducer(client)
+
+	time.Sleep(10 * time.Second)
+
+	fmt.Println("send msgs:")
+	fmt.Println(strings.Join(sendMsgs, "; "))
+	fmt.Println("recv msgs:")
+	fmt.Println(strings.Join(recvMsgs, "; "))
 }
 
 func TestAck(t *testing.T) {
